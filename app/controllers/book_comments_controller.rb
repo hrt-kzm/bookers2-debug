4ce2aa13f700f7860d6453a6d593307a63272632
EC2 Default User <ec2-user@ip-172-31-22-83.ap-northeast-1.compute.internal>
2022-10-31 07:50:24 +0000

    [Add]コメント機能・非同期化

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -5,12 +5,13 @@ class BookCommentsController < ApplicationController
     comment = current_user.book_comments.new(book_comment_params)
     comment.book_id = book.id
     comment.save
-    redirect_back(fallback_location: root_path) #redirect_to request.referer・・・こういう書き方もある
+    #redirect_back(fallback_location: root_path) #redirect_to request.referer・・・こういう書き方もある
   end
 
   def destroy
     BookComment.find(params[:id]).destroy
-    redirect_back(fallback_location: root_path)
+    #redirect_back(fallback_location: root_path)
+    @book = Book.find(params[:book_id])
   end
 
   private
